76cae710193e748bc91161b4063f842a976560b6
shayanbayersin-arch <shayanbayersin@gmail.com>
2026-03-16 00:42:37 +0500

    Add Singleton GameConfigure

--- src/com/narxoz/rpg/singleton/GameConfig.java
+++ src/com/narxoz/rpg/singleton/GameConfig.java
@@ -1,5 +1,18 @@
 package com.narxoz.rpg.singleton;
 
 public class GameConfig {
+    private static GameConfig instance;
+
+    private GameConfig() {
+      
+    }
+
+    public static GameConfig getInstance() {
+        if (instance == null) {
+            instance = new GameConfig();
+        }
+        return instance;
+    }
+
     
 }
