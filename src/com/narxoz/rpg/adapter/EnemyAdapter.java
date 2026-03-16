b87acfc9e47210b98a555edfb59afdccfc01a0ed
shayanbayersin-arch <shayanbayersin@gmail.com>
2026-03-16 14:26:09 +0500

    Implement EnemyAdapter class to adapt EnemyUnit as a Combatant

--- src/com/narxoz/rpg/adapter/EnemyAdapter.java
+++ src/com/narxoz/rpg/adapter/EnemyAdapter.java
@@ -1,5 +1,27 @@
 package com.narxoz.rpg.adapter;
 
-public class EnemyAdapter {
-    
+import com.narxoz.rpg.composite.EnemyUnit;
+
+public class EnemyAdapter implements Combatant {
+
+    private EnemyUnit enemy;
+
+    public EnemyAdapter(EnemyUnit enemy) {
+        this.enemy = enemy;
+    }
+
+    @Override
+    public String getName() {
+        return enemy.getName();
+    }
+
+    @Override
+    public int getHealth() {
+        return enemy.getHealth();
+    }
+
+    @Override
+    public void takeDamage(int damage) {
+        enemy.takeDamage(damage);
+    }
 }
