3a109e09acac877509836ac287fce2254862c868
shayanbayersin-arch <shayanbayersin@gmail.com>
2026-03-16 14:12:34 +0500

    Implement HeroAdapter class to adapt HeroUnit as a Combatant

--- src/com/narxoz/rpg/adapter/HeroAdapter.java
+++ src/com/narxoz/rpg/adapter/HeroAdapter.java
@@ -1,5 +1,27 @@
 package com.narxoz.rpg.adapter;
 
-public class HeroAdapter {
-    
+import com.narxoz.rpg.composite.HeroUnit;
+
+public class HeroAdapter implements Combatant {
+
+    private HeroUnit hero;
+
+    public HeroAdapter(HeroUnit hero) {
+        this.hero = hero;
+    }
+
+    @Override
+    public String getName() {
+        return hero.getName();
+    }
+
+    @Override
+    public int getHealth() {
+        return hero.getHealth();
+    }
+
+    @Override
+    public void takeDamage(int amount) {
+        hero.takeDamage(amount);
+    }
 }
