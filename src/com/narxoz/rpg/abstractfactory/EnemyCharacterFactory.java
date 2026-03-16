453fbe551f7645785cfc584aecbfe2c6c7529a3f
shayanbayersin-arch <shayanbayersin@gmail.com>
2026-03-16 14:31:28 +0500

    Implement EnemyCharacterFactory and HeroCharacterFactory to create HeroUnit and EnemyUnit instances

--- src/com/narxoz/rpg/abstractfactory/EnemyCharacterFactory.java
+++ src/com/narxoz/rpg/abstractfactory/EnemyCharacterFactory.java
@@ -1,5 +1,21 @@
 package com.narxoz.rpg.abstractfactory;
 
-public class EnemyCharacterFactory {
-    
+import com.narxoz.rpg.composite.HeroUnit;
+import com.narxoz.rpg.composite.EnemyUnit;
+
+public class EnemyCharacterFactory implements CharacterFactory {
+
+    @Override
+    public HeroUnit createHero(String name) {
+
+        return new HeroUnit(name,100,20);
+
+    }
+
+    @Override
+    public EnemyUnit createEnemy(String name) {
+
+        return new EnemyUnit(name,140,30);
+
+    }
 }
--- src/com/narxoz/rpg/abstractfactory/HeroCharacterFactory.java
+++ src/com/narxoz/rpg/abstractfactory/HeroCharacterFactory.java
@@ -1,5 +1,21 @@
 package com.narxoz.rpg.abstractfactory;
 
-public class HeroCharacterFactory {
-    
+import com.narxoz.rpg.composite.HeroUnit;
+import com.narxoz.rpg.composite.EnemyUnit;
+
+public class HeroCharacterFactory implements CharacterFactory {
+
+    @Override
+    public HeroUnit createHero(String name) {
+
+        return new HeroUnit(name,120,25);
+
+    }
+
+    @Override
+    public EnemyUnit createEnemy(String name) {
+
+        return new EnemyUnit(name,80,20);
+
+    }
 }
