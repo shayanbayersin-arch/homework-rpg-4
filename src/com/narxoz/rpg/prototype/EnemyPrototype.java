f88b8f54ef947c1de8cc24e51f54574e071750dd
shayanbayersin-arch <shayanbayersin@gmail.com>
2026-03-16 14:08:57 +0500

    Implement EnemyPrototype class with cloning functionality

--- src/com/narxoz/rpg/prototype/EnemyPrototype.java
+++ src/com/narxoz/rpg/prototype/EnemyPrototype.java
@@ -1,5 +1,16 @@
 package com.narxoz.rpg.prototype;
 
-public class EnemyPrototype {
-    
+import com.narxoz.rpg.composite.EnemyUnit;
+
+public class EnemyPrototype implements Cloneable {
+
+    private EnemyUnit enemy;
+
+    public EnemyPrototype(EnemyUnit enemy) {
+        this.enemy = enemy;
+    }
+
+    public EnemyUnit cloneEnemy() {
+        return new EnemyUnit(enemy.getName(), enemy.getHealth(), enemy.getAttackPower());
+    }
 }
